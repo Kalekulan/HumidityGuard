4c0c05370ca5a08a038a75bf0a5c8910a077c6da
Kalekulan <lukas.foughman@gmail.com>
2017-07-07 18:57:05 +0200

    Beautified

--- HumidityGuard.ino
+++ HumidityGuard.ino
@@ -6,6 +6,7 @@ struct RGB {
 	int blue;
 };
 
+boolean const DEBUG = false;
 int const NUMBEROFMODULES = 2;	//Change this variable as to how many water sensors and led modules you have (you need one of each)
 
 RGB const ledmodule[NUMBEROFMODULES] = {	//You also needs to add number of rows equal to NUMBEROFMODULES
@@ -19,22 +20,22 @@ int STATE = 0;
 
 void setup(){
 	int r = 0;
-	Serial.begin(9600);
+	if(DEBUG) Serial.begin(9600);
 	//pinMode(ledmodule[0].red, OUTPUT);
-	//pinMode(ledmodule[0].green, OUTPUT); 
-	//pinMode(ledmodule[0].blue, OUTPUT);  
+	//pinMode(ledmodule[0].green, OUTPUT);
+	//pinMode(ledmodule[0].blue, OUTPUT);
 	for(r = 0; r < NUMBEROFMODULES; r++) {	//Setting pinmode=OUTPUT for leds
 		pinMode(ledmodule[r].red, OUTPUT);
-		pinMode(ledmodule[r].green, OUTPUT); 
-		pinMode(ledmodule[r].blue, OUTPUT);  
-		
+		pinMode(ledmodule[r].green, OUTPUT);
+		pinMode(ledmodule[r].blue, OUTPUT);
+
 		TurnOffLeds(&ledmodule[r]);
 	}
 		//TurnOffLeds(ledmodule[r].red, ledmodule[r].green, ledmodule[r].blue);
-	Serial.println("Starting up...");
+	if(DEBUG) Serial.println("Starting up...");
 
-	
-	for(r = 0; r<= 4; r++) {	//Startup blinking led sequence
+
+	for(r = 0; r <= 4; r++) {	//Startup blinking led sequence
 		for(int i = 0; i < NUMBEROFMODULES; i++) {
 			digitalWrite(ledmodule[i].blue, HIGH);
 			delay(200);
@@ -44,7 +45,7 @@ void setup(){
 			digitalWrite(ledmodule[i].red, LOW);
 			digitalWrite(ledmodule[i].green, HIGH);
 			delay(200);
-			digitalWrite(ledmodule[i].green, LOW);	  
+			digitalWrite(ledmodule[i].green, LOW);
 		}
 	}
 }
@@ -58,38 +59,38 @@ void loop(){
 	int moduleCounter = 0;
 	int readCounter = 0;
 	int rSetStatus;
-	
-	
+
+
 	switch (STATE){
 		case READ:	//Will stay in this loop for 60sec then go to POWERSAVE
-			Serial.print("STATE=");
-			Serial.println(STATE);
-			
+			if(DEBUG) Serial.print("STATE=");
+			if(DEBUG) Serial.println(STATE);
+
 			for(readCounter = 0; readCounter < numReadSets; readCounter++){ 	//The number of readings
 				for(moduleCounter = 0; moduleCounter < NUMBEROFMODULES; moduleCounter++) {	//Read every module
 					waterLevel[moduleCounter] = WaterLevelRead(sensorPins[moduleCounter], numReads);
-					Serial.print("waterLevel[");
-					Serial.print(moduleCounter);
-					Serial.print("]=");
-					Serial.println(waterLevel[moduleCounter]);
+					if(DEBUG) Serial.print("waterLevel[");
+					if(DEBUG) Serial.print(moduleCounter);
+					if(DEBUG) Serial.print("]=");
+					if(DEBUG) Serial.println(waterLevel[moduleCounter]);
 					if(readCounter != 0) rSetStatus = SetStatus(&ledmodule[moduleCounter], waterLevel[moduleCounter]); //Skipping first read since there's some current rush going on...
-					Serial.print("rSetStatus=");
-					Serial.println(rSetStatus);
+					if(DEBUG) Serial.print("rSetStatus=");
+					if(DEBUG) Serial.println(rSetStatus);
 				}
 				delay(3500); //3.5 secs
-				//Serial.print("readCounter=");
-				//Serial.println(readCounter);
+				//if(DEBUG) Serial.print("readCounter=");
+				//if(DEBUG) Serial.println(readCounter);
 			}
-			
 
-			STATE=POWERSAVE;	//Read is done. Go to sleep!
+
+			STATE = POWERSAVE;	//Read is done. Go to sleep!
 			break;
 		case POWERSAVE:
-			Serial.print("STATE=");
-			Serial.println(STATE);
-			for(int r=0; r < NUMBEROFMODULES; r++) TurnOffLeds(&ledmodule[r]);
+			if(DEBUG) Serial.print("STATE=");
+			if(DEBUG) Serial.println(STATE);
+			for(int r = 0; r < NUMBEROFMODULES; r++) TurnOffLeds(&ledmodule[r]);
 				//TurnOffLeds(ledmodule[r].red, ledmodule[r].green, ledmodule[r].blue);
-			Serial.println("Powering down. Forever...");
+			if(DEBUG) Serial.println("Powering down. Forever...");
 			delay(1000);
 			LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 			break;
@@ -102,113 +103,74 @@ void loop(){
 }
 
 int WaterLevelRead(int const sensorPin, int const numReads){
-	
+
 	int sensorvalue[numReads];
 	int avgRead = 0;
-	
+
 	for(int i = 0; i < numReads; i++){
 		sensorvalue[i] = analogRead(sensorPin);  //Read the analog value
-		Serial.print("WaterLevelRead::sensorvalue[");
-		Serial.print(i);
-		Serial.print("]=");
-		Serial.println(sensorvalue[i]);  //Print the analog value
-		avgRead = avgRead + sensorvalue[i]; 
-		delay(500); 
+		if(DEBUG) Serial.print("WaterLevelRead::sensorvalue[");
+		if(DEBUG) Serial.print(i);
+		if(DEBUG) Serial.print("]=");
+		if(DEBUG) Serial.println(sensorvalue[i]);  //Print the analog value
+		avgRead = avgRead + sensorvalue[i];
+		delay(500);
 	}
 
 		avgRead = avgRead / numReads;	//Average value of the readings
-		Serial.print("WaterLevelRead::avgRead=");
-		Serial.println(avgRead);
+		if(DEBUG) Serial.print("WaterLevelRead::avgRead=");
+		if(DEBUG) Serial.println(avgRead);
 
 	return avgRead;
 }
 
 int SetStatus(const struct RGB *leds, int value){
-	
+
 	int const SAFE = 200;		//SAFE is between 0 -> 100
 	int const WARNING = 600;	//WARNING is between 101 -> 600
 	int const DANGER = 1023;	//DANGER is between 601 -> 1023
-	
-	switch(value) {	
+
+	switch(value) {
 		case 0 ...SAFE: 	//GREEN LED
-			digitalWrite(leds->blue, LOW);  
-			digitalWrite(leds->red, LOW);  
-			digitalWrite(leds->green, HIGH);  
-			Serial.print("SetStatus::");
-			Serial.println("GREEN");
+			digitalWrite(leds->blue, LOW);
+			digitalWrite(leds->red, LOW);
+			digitalWrite(leds->green, HIGH);
+			if(DEBUG) Serial.print("SetStatus::");
+			if(DEBUG) Serial.println("GREEN");
 			break;
-		
+
 		case SAFE+1 ...WARNING: 	//BLUE LED
-			digitalWrite(leds->green, LOW);  
-			digitalWrite(leds->red, LOW);  
-			digitalWrite(leds->blue, HIGH);  
-			Serial.print("SetStatus::");
-			Serial.println("BLUE");
+			digitalWrite(leds->green, LOW);
+			digitalWrite(leds->red, LOW);
+			digitalWrite(leds->blue, HIGH);
+			if(DEBUG) Serial.print("SetStatus::");
+			if(DEBUG) Serial.println("BLUE");
 			break;
-			
+
 		case WARNING+1 ...DANGER: 	//RED LED
-			digitalWrite(leds->green, LOW);  
-			digitalWrite(leds->blue, LOW);  
-			digitalWrite(leds->red, HIGH);  		
-			Serial.print("SetStatus::");
-			Serial.println("RED");
+			digitalWrite(leds->green, LOW);
+			digitalWrite(leds->blue, LOW);
+			digitalWrite(leds->red, HIGH);
+			if(DEBUG) Serial.print("SetStatus::");
+			if(DEBUG) Serial.println("RED");
 			break;
-			
+
 		default:	//Value not in range
-			digitalWrite(leds->green, LOW);  
-			digitalWrite(leds->blue, LOW); 
-			digitalWrite(leds->red, LOW); 	
-			Serial.print("SetStatus::");
-			Serial.println("COLORLESS");			
+			digitalWrite(leds->green, LOW);
+			digitalWrite(leds->blue, LOW);
+			digitalWrite(leds->red, LOW);
+			if(DEBUG) Serial.print("SetStatus::");
+			if(DEBUG) Serial.println("COLORLESS");
 			return false;
 	}
-/* 	if(value >= SAFE && value <= WARNING){  //Compare analog value with threshold 
-		//ledmodule[0] = (RGB){LOW, LOW, HIGH};
-		digitalWrite(ledmodule[0].green, LOW);  
-		digitalWrite(ledmodule[0].red, LOW);  
-		digitalWrite(ledmodule[0].blue, HIGH);  
-
-		Serial.print("SetStatus::");
-	}  
-	else if(value > DANGER){ 
-		//ledmodule[0] = (RGB){HIGH, LOW, LOW};
-		digitalWrite(ledmodule[0].green, LOW);  
-		digitalWrite(ledmodule[0].blue, LOW);  
-		digitalWrite(ledmodule[0].red, HIGH);  
-	} 
-	else if(value < SAFE){
-		//ledmodule[0] = (RGB){LOW, HIGH, LOW};
-		digitalWrite(ledmodule[0].blue, LOW);  
-		digitalWrite(ledmodule[0].red, LOW);  
-		digitalWrite(ledmodule[0].green, HIGH);  
-	
-	}
-	else{
-		Serial.print("SetStatus::value=");
-		Serial.println(value);
-		return false;
-	} */
 	return true;
 }
 
 void TurnOffLeds(const struct RGB *leds){
-	Serial.println("Turning off LED module");
+	if(DEBUG) Serial.println("Turning off LED module");
 
-	digitalWrite(leds->blue, LOW);  
-	digitalWrite(leds->red, LOW);  
-	digitalWrite(leds->green, LOW);   
+	digitalWrite(leds->blue, LOW);
+	digitalWrite(leds->red, LOW);
+	digitalWrite(leds->green, LOW);
 
 }
-
-/*
-void TurnOffLeds(int red, int green, int blue){
-	Serial.println("Turning off LED module");
-
-/* 	digitalWrite(ledmodule[0].blue, LOW);  
-	digitalWrite(ledmodule[0].red, LOW);  
-	digitalWrite(ledmodule[0].green, LOW);   
-	digitalWrite(red, LOW); 
-	digitalWrite(green, LOW); 
-	digitalWrite(blue, LOW);  
-}
- */
